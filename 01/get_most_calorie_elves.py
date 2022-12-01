2c907354d48ecfb105e7e33a3db0452fc368cd54
HowToBeCalculated <HowToBeCalculated@outlook.com>
2022-12-01 18:32:21 -0500

    a little touching up of day 1 to separate concerns

--- 01/get_most_calorie_elves.py
+++ 01/get_most_calorie_elves.py
@@ -2,11 +2,8 @@
 
 def get_top_x_elves_with_most_calories(
     top_num_of_elves: int,
-    input_file_name: str = "01_input.txt"
+    list_of_calories: list[str],
 ) -> list[int]:
-
-    with open(input_file_name, "r") as f:
-        list_of_calories = f.read().splitlines()
 
     all_weights = []
     weight = 0
@@ -19,11 +16,14 @@ def get_top_x_elves_with_most_calories(
             all_weights.append(weight)
             weight = 0
 
-    return sorted(all_weights)[-top_num_of_elves:]
+    return sorted(all_weights, reverse=True)[:top_num_of_elves]
 
 if __name__ == "__main__":
 
-    top_1_elves_calories = sum(get_top_x_elves_with_most_calories(1))
-    top_3_elves_calories = sum(get_top_x_elves_with_most_calories(3))
+    with open("01_input.txt", "r") as f:
+        list_of_calories = f.read().splitlines()
+
+    top_1_elves_calories = sum(get_top_x_elves_with_most_calories(1, list_of_calories[:]))
+    top_3_elves_calories = sum(get_top_x_elves_with_most_calories(3, list_of_calories[:]))
 
     print(f"{top_1_elves_calories=}", f"{top_3_elves_calories=}", sep="\n")
